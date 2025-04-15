a9fbe0debce7ccb81e3ebd323b172633cd763f6f
Neil Enns <neile@live.com>
2025-04-15 12:58:24 +0000

    Devcontainer and nesting

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   server: {
     port: 4000,
+    host: "127.0.0.1", // Required to access the server when it is running in a devcontainer.
   },
 });
